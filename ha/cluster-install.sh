6a971b2d1db65450d3fd4120eb65d83946793840
viyancs <msofyancs@gmail.com>
2023-12-15 07:56:49 +0700

    update doc and rancher configuration

--- ha/cluster-install.sh
+++ ha/cluster-install.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
+# Check if Docker is installed
+if ! command -v docker &> /dev/null; then
+    # Install Docker prerequisites
+    sudo apt update
+    sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+    
+    # Add Docker's official GPG key
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+    # Add Docker repository for Ubuntu
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+    # Update package index again
+    sudo apt update
+
+    # Install Docker
+    sudo apt install -y docker-ce
+
+    # Add current user to the docker group to run Docker commands without sudo
+    sudo usermod -aG docker $USER
+
+    echo "Docker has been installed successfully."
+else
+    echo "Docker is already installed."
+fi
+
 # update package
 sudo apt update -y && sudo apt install curl -y
 
